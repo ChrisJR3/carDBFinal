7a2be4de1767786f39065af7d2e58e3626424e68
Chris Rouse <chris.rouse03@gmail.com>
2020-05-18 13:12:20 -0400

    Add project files.

---
+++ carDBFinal/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace carDBFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.mileageInput = new System.Windows.Forms.TextBox();
+            this.colourInput = new System.Windows.Forms.TextBox();
+            this.makeInput = new System.Windows.Forms.TextBox();
+            this.yearInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(170, 122);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(64, 23);
+            this.exitButton.TabIndex = 21;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Mileage";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Colour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Make";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Year";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(12, 170);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(300, 271);
+            this.outputLabel.TabIndex = 16;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(72, 122);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(64, 23);
+            this.addButton.TabIndex = 15;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // mileageInput
+            // 
+            this.mileageInput.Location = new System.Drawing.Point(72, 89);
+            this.mileageInput.Name = "mileageInput";
+            this.mileageInput.Size = new System.Drawing.Size(127, 20);
+            this.mileageInput.TabIndex = 14;
+            // 
+            // colourInput
+            // 
+            this.colourInput.Location = new System.Drawing.Point(72, 63);
+            this.colourInput.Name = "colourInput";
+            this.colourInput.Size = new System.Drawing.Size(127, 20);
+            this.colourInput.TabIndex = 13;
+            // 
+            // makeInput
+            // 
+            this.makeInput.Location = new System.Drawing.Point(72, 37);
+            this.makeInput.Name = "makeInput";
+            this.makeInput.Size = new System.Drawing.Size(127, 20);
+            this.makeInput.TabIndex = 12;
+            // 
+            // yearInput
+            // 
+            this.yearInput.Location = new System.Drawing.Point(72, 11);
+            this.yearInput.Name = "yearInput";
+            this.yearInput.Size = new System.Drawing.Size(127, 20);
+            this.yearInput.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.mileageInput);
+            this.Controls.Add(this.colourInput);
+            this.Controls.Add(this.makeInput);
+            this.Controls.Add(this.yearInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox mileageInput;
+        private System.Windows.Forms.TextBox colourInput;
+        private System.Windows.Forms.TextBox makeInput;
+        private System.Windows.Forms.TextBox yearInput;
+    }
+}
+
---
+++ carDBFinal/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+using CarInventory;
+
+namespace carDBFinal
+{
+    public partial class Form1 : Form
+    {
+        List<Car> carsList = new List<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            readerMethod();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            writerMethod();
+        }
+
+        public void readerMethod()
+        {
+            string newYear, newMake, newColour, newMileage;
+
+            XmlReader reader = XmlReader.Create("Resources/carXMLFile.xml");
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+                    reader.ReadToNextSibling("year");
+                    newYear = reader.ReadString();
+
+                    reader.ReadToNextSibling("make");
+                    newMake = reader.ReadString();
+
+                    reader.ReadToNextSibling("mileage");
+                    newColour = reader.ReadString();
+
+                    reader.ReadToNextSibling("colour");
+                    newMileage = reader.ReadString();
+
+                    Car newCar = new Car(newYear, newMake, newColour, newMileage);
+                    carsList.Add(newCar);
+                }
+            }
+
+            reader.Close();
+        }
+        public void writerMethod()
+        {
+            XmlWriter writer = XmlWriter.Create("Resources/carXMLFile", null);
+
+            writer.WriteStartElement("Cars:");
+
+            foreach (Car c in carsList)
+            {
+                writer.WriteStartElement("Car");
+
+                writer.WriteElementString("Year", c.year);
+                writer.WriteElementString("Make", c.make);
+                writer.WriteElementString("Mileage", c.mileage);
+                writer.WriteElementString("Colour", c.colour);
+
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+
+            writer.Close();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string year, make, colour, mileage;
+
+            year = yearInput.Text;
+            make = makeInput.Text;
+            colour = colourInput.Text;
+            mileage = mileageInput.Text;
+
+            Car car = new Car(year, make, colour, mileage);
+
+            carsList.Add(car);
+
+            outputLabel.Text = "";
+
+            foreach (Car c in carsList)
+            {
+                outputLabel.Text += c.year + " "
+                    + c.make + " "
+                    + c.colour + " "
+                    + c.mileage + "\n";
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
